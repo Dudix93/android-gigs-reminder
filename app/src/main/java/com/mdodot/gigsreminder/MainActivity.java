7c9de065615bd9c7d7b6df61d30ee84812e7f8bd
Michał Dodot <dodo45693@gmail.com>
2019-12-04 22:27:28 +0100

    Simple list

--- app/src/main/java/com/mdodot/gigsreminder/MainActivity.java
+++ app/src/main/java/com/mdodot/gigsreminder/MainActivity.java
@@ -3,12 +3,29 @@ package com.mdodot.gigsreminder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView listView;
+    private String[] gigsList = new String[]{
+            "As I Lay Dying",
+            "Currents",
+            "Counterparts",
+            "Fit For A King",
+            "Bury Tomorrow",
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.list);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, R.layout.array_list_item, R.id.array_element, gigsList);
+        listView.setAdapter(arrayAdapter);
     }
+
 }
