f00611bd10779c07233375c02c924e2c5550caa7
Michal Dodot <dodo45693@gmail.com>
2020-12-22 10:50:50 +0000

    Delete venue dialog

--- app/src/main/java/com/mdodot/gigsreminder/VenuesAdapter.java
+++ app/src/main/java/com/mdodot/gigsreminder/VenuesAdapter.java
@@ -1,12 +1,19 @@
 package com.mdodot.gigsreminder;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 
 import java.util.ArrayList;
 
@@ -56,7 +63,11 @@ public class VenuesAdapter extends ArrayAdapter<VenueModel>  {
             @Override
             public void onClick(View view) {
                 if (mContext instanceof VenuesActivity) {
-                    ((VenuesActivity) mContext).deleteVenue(venueId);
+                    DialogFragment deleteVenueFragment = new DeleteVenueDialogFragment();
+                    Bundle args = new Bundle();
+                    args.putInt("venueId", venueId);
+                    deleteVenueFragment.setArguments(args);
+                    deleteVenueFragment.show(((VenuesActivity) mContext).getSupportFragmentManager(), "deleteVenue");
                 }
             }
         });
@@ -72,5 +83,29 @@ public class VenuesAdapter extends ArrayAdapter<VenueModel>  {
         });
 
         return convertView;
+    }
+    
+    public static class DeleteVenueDialogFragment extends DialogFragment {
+        private int venueId;
+
+        @Override
+        public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            venueId = getArguments().getInt("venueId");
+            builder.setMessage(R.string.event_delete)
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ((VenuesActivity)getActivity()).deleteVenue(venueId);
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            //User cancelled the dialog
+                        }
+                    });
+            return builder.create();
+        }
     }
 }
