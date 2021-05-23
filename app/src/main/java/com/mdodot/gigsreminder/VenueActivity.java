20de9382737e27421a7f0d578926e47abd1b0bf4
Michal Dodot <dodo45693@gmail.com>
2021-05-23 17:53:38 +0100

    Getting directions to a venue via Google Maps

--- app/src/main/java/com/mdodot/gigsreminder/VenueActivity.java
+++ app/src/main/java/com/mdodot/gigsreminder/VenueActivity.java
@@ -2,18 +2,24 @@ package com.mdodot.gigsreminder;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -24,6 +30,9 @@ import com.google.android.libraries.places.api.net.FetchPlaceRequest;
 import com.google.android.libraries.places.api.net.FetchPlaceResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
 
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
@@ -40,6 +49,7 @@ public class VenueActivity extends FragmentActivity implements OnMapReadyCallbac
     private TextView venueTownTextView;
     private SupportMapFragment mapFragment;
     private List<Place.Field> placeFields;
+    private UiSettings mUiSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,9 +69,9 @@ public class VenueActivity extends FragmentActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng coordinations = place.getLatLng();
-        mMap.addMarker(new MarkerOptions().position(coordinations));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinations, 16.0f));
+        mUiSettings = mMap.getUiSettings();
+        mUiSettings.setZoomControlsEnabled(true);
+        setLocation();
     }
 
     public void fetchPlaceDetails() {
@@ -81,6 +91,23 @@ public class VenueActivity extends FragmentActivity implements OnMapReadyCallbac
             public void onSuccess(FetchPlaceResponse response) {
                 place = response.getPlace();
                 mapFragment.getMapAsync((OnMapReadyCallback) mapFragment.getContext());
+                findViewById(R.id.map_get_directions).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        try {
+                            URL mapsURL = new URL("https://www.google.com/maps/dir/?api=1");
+                            String uri = String.format(Locale.ENGLISH, mapsURL.toString().concat(String.format("&dir_action=navigate&destination=%s&destination_place_id=%s",
+                                    place.getAddress(),
+                                    place.getId())));
+                            Intent googleMapsActivity = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                            googleMapsActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            mContext.startActivity(googleMapsActivity);
+                        }
+                        catch (MalformedURLException e) {
+                            Toast.makeText(mContext, e.getMessage() + "", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
@@ -90,5 +117,11 @@ public class VenueActivity extends FragmentActivity implements OnMapReadyCallbac
                 }
             }
         });
+    }
+
+    public void setLocation() {
+        LatLng coordinations = place.getLatLng();
+        mMap.addMarker(new MarkerOptions().position(coordinations));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(coordinations, 16.0f));
     }
 }
