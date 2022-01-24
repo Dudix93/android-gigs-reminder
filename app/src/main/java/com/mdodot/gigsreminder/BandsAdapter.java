70254f97ae10bd388c45968cf4596c2ec03b700e
Michal Dodot <dodo45693@gmail.com>
2022-01-24 20:55:30 +0000

    Event screen layout polished

--- app/src/main/java/com/mdodot/gigsreminder/BandsAdapter.java
+++ app/src/main/java/com/mdodot/gigsreminder/BandsAdapter.java
@@ -1,8 +1,8 @@
 package com.mdodot.gigsreminder;
 
+import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,20 +10,21 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.fragment.app.DialogFragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
 public class BandsAdapter extends ArrayAdapter<BandModel>  {
     private ArrayList<BandModel> dataSet;
-    Context mContext;
+    Activity mContext;
 
     private static class ViewHolder {
         TextView band;
     }
 
-    public BandsAdapter(ArrayList<BandModel> data, Context context){
-        super(context, R.layout.array_list_item_simple, data);
+    public BandsAdapter(ArrayList<BandModel> data, Activity context){
+        super(context, R.layout.array_list_item_w_deletion, data);
         this.dataSet = data;
         this.mContext = context;
     }
@@ -37,7 +38,7 @@ public class BandsAdapter extends ArrayAdapter<BandModel>  {
         if (convertView == null) {
             viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.array_list_item_simple, parent, false);
+            convertView = inflater.inflate(R.layout.array_list_item_w_deletion, parent, false);
             viewHolder.band = convertView.findViewById(R.id.array_list_simple_item);
             convertView.setTag(viewHolder);
         } else {
@@ -46,16 +47,22 @@ public class BandsAdapter extends ArrayAdapter<BandModel>  {
 
         viewHolder.band.setText(bandModel.getBandName());
 
-        convertView.findViewById(R.id.deleteListItemIcon).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (!dataSet.isEmpty()) {
-                    AddEvent.supportsList.remove(bandModel);
-                    AddEvent.supportsAdapter.notifyDataSetChanged();
-                    AddEvent.supportsForDeletion.add(bandModel);
+        if (getContext().getClass() == AddEvent.class) {
+            convertView.findViewById(R.id.deleteListItemIcon).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (!dataSet.isEmpty()) {
+                        AddEvent.supportsList.remove(bandModel);
+                        AddEvent.supportsAdapter.notifyDataSetChanged();
+                        AddEvent.supportsForDeletion.add(bandModel);
+                    }
                 }
-            }
-        });
+            });
+        }
+        else {
+            convertView.findViewById(R.id.deleteListItemIcon).setVisibility(View.INVISIBLE);
+            viewHolder.band.setGravity(Gravity.CENTER_HORIZONTAL);
+        }
 
         return convertView;
     }
