8479f7c65d1bc679b7b2069ff41c00bc63773d34
Michal Dodot <dodo45693@gmail.com>
2022-01-28 19:33:12 +0000

    Handling empty supports list on event screen

--- app/src/main/java/com/mdodot/gigsreminder/LineupListFragment.java
+++ app/src/main/java/com/mdodot/gigsreminder/LineupListFragment.java
@@ -8,27 +8,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class LineupListFragment extends Fragment {
 
-    private ArrayList<BandModel> supportsList;
+    private final ArrayList<BandModel> supportsList;
 
     public LineupListFragment(ArrayList<BandModel> supportsList) {
         this.supportsList = supportsList;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View layoutView = inflater.inflate(R.layout.lineup_listview, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        ListView supportsListView = layoutView.findViewById(R.id.supportsListView);
-
-        BandsAdapter supportsAdapter = new BandsAdapter(this.supportsList, getContext());
-        supportsListView.setAdapter(supportsAdapter);
-
-        return layoutView;
+        if (supportsList.size() > 0) {
+            View layoutView = inflater.inflate(R.layout.lineup_listview, container, false);
+            ListView supportsListView = layoutView.findViewById(R.id.supportsListView);
+            BandsAdapter supportsAdapter = new BandsAdapter(this.supportsList, getContext());
+            supportsListView.setAdapter(supportsAdapter);
+            return layoutView;
+        }
+        else {
+            View layoutView = inflater.inflate(R.layout.screen_message, container, false);
+            TextView screenMessageTextView = layoutView.findViewById(R.id.screen_text_message);
+            screenMessageTextView.setText(getResources().getText(R.string.no_supports));
+            return layoutView;
+        }
     }
 }
